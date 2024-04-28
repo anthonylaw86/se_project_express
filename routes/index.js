d80ce3a7dc7499bf833d3a2453741b08dbedd394
AnthonyLaw <anthonylaw910@gmail.com>
2024-04-28 11:14:51 -0600

    project 12 submission 2

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { INTERNAL_SERVER_ERROR, NOT_FOUND_ERROR } = require("../utils/errors");
+const { NOT_FOUND_ERROR } = require("../utils/errors");
 
 const clothingItemRouter = require("./clothingItem");
 const userRouter = require("./users");
@@ -8,12 +8,7 @@ router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
 
 router.use((req, res) => {
-  if (err.name === "DocumentNotFoundError") {
-    return res.status(NOT_FOUND_ERROR).send({ message: err.message });
-  }
-  return res
-    .status(INTERNAL_SERVER_ERROR)
-    .send({ message: "Router not found" });
+  res.status(NOT_FOUND_ERROR).send({ message: "Router not found" });
 });
 
 module.exports = router;
