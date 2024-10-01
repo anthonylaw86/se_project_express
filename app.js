82f408c68cba261eec3599c161e01acb311027d9
AnthonyLaw <anthonylaw910@gmail.com>
2024-09-30 21:30:20 -0600

    adj

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const app = express();
 const { PORT = 3001 } = process.env;
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .connect("mongodb://127.0.0.1:27017/wtwr_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to DB");
   })
